4cb7056ba51edad0f2bcc62fadda2fa0ac9b8f2c
Joshua Banjo <joshuabanjo.tech@gmail.com>
2024-09-26 15:10:04 +0100

    Implemented both Read operations to show all task and one task

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Task.destroy_all
+
+puts "Creating tasks..."
+task_1 = { title: "Complete outstanding challenges", details: "Slowly getting back on track but need to maintain momentum", completed: false }
+task_2 = { title: "Hit today's full body workout", details: "Increase trapbar deadlift amount by 20kg to go for a PR", completed: false }
+task_3 = { title: "Call my mother", details: "Forgot to call on the weekend; need to provide her with update on life", completed: false }
+task_4 = { title: "Rotate attention towards career workbook for Milly", details: "Currently in Phase 2 but many things to do", completed: false }
+task_5 = { title: "Send RSVP for upcoming mate's wedding", details: "Banji's sister is getting married and I would like to attend", completed: false }
+
+[ task_1, task_2, task_3, task_4, task_5 ].each do |attributes|
+  task = Task.create!(attributes)
+  puts "Created #{task.title}"
+end
+puts "Finished!"
